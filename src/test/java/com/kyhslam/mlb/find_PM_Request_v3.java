5109485e9c3bdecf5e9987e04060d3895360c08d
kyhslam <kyhslam@gmail.com>
2025-11-28 15:05:43 +0900

    [2025-11-28] 김선태 요청 건

--- src/test/java/com/kyhslam/mlb/find_PM_Request_v3.java
+++ src/test/java/com/kyhslam/mlb/find_PM_Request_v3.java
@@ -52,7 +52,7 @@ public class find_PM_Request_v3 {
             // 첫 번째 행(헤더)은 건너뛴다고 가정 (row 0은 헤더)
             //for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
             //for (int rowIndex = 5001; rowIndex <= 8000; rowIndex++) {
-            for (int rowIndex = 7045; rowIndex <= 12000; rowIndex++) {
+            for (int rowIndex = 12500; rowIndex <= 12600; rowIndex++) {
 
                 System.out.println("excel row == " + rowIndex);
 
