ed1af880da692e3f49347e1fbd343d6fcbfea6e4
kyhslam <kyhslam@gmail.com>
2025-12-02 15:53:35 +0900

    [2025-12-02] api 개발

--- src/main/java/com/kyhslam/controller/APIController.java
+++ src/main/java/com/kyhslam/controller/APIController.java
@@ -2,14 +2,15 @@ package com.kyhslam.controller;
 
 import com.kyhslam.dto.PartInfoDTO;
 import com.kyhslam.service.MLBService;
+import com.kyhslam.service.SubaeService;
+import com.kyhslam.util.MLBCommonUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Description;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +19,11 @@ public class APIController {
 
     private final MLBService mlbService;
 
+    private final SubaeService subaeService;
+
     @Description("수량 PID 조회 로직")
     @PostMapping("/api/searchPartQtyPid")
+    @CrossOrigin
     @ResponseBody
     public ArrayList<PartInfoDTO> searchPartQtyPid(String year, String blockNo, String qtyPid, String key) {
         ArrayList<PartInfoDTO> resultList = new ArrayList<>();
@@ -29,4 +33,50 @@ public class APIController {
 
         return resultList;
     }
+
+    @Description("품번으로 속성정보 조회")
+    @CrossOrigin
+    @GetMapping("/api/findPartOneWithPartNo")
+    @ResponseBody
+    public PartInfoDTO findPartOneWithPartNo(String partNo,String key) {
+        PartInfoDTO result =  new PartInfoDTO();
+
+        if ("subae".equals(key)) {
+            result = MLBCommonUtil.findPartOneWithPartNo(partNo);
+        }
+
+        return result;
+    }
+
+    //품번으로 하위 BOM 조회
+    @Description("품번으로 하위 BOM 조회")
+    @CrossOrigin
+    @GetMapping("/api/findAssyDownBOM")
+    @ResponseBody
+    public ArrayList<PartInfoDTO> findAssyDownBOM(String partNo, String key) {
+
+        ArrayList<PartInfoDTO> result = new ArrayList<>();
+        if ("subae".equals(key)) {
+            result = MLBCommonUtil.findAssyDownBOM(partNo);
+        }
+
+        return result;
+    }
+
+
+    @Description("시물레이터 결과만 추출")
+    @GetMapping("/api/pidExecute")
+    @ResponseBody
+    @CrossOrigin
+    public HashMap<String, String> pidExecute(String pid, String hogi, String testVersion,
+                                              String floor, String isfloor, String key) {
+
+        HashMap<String, String> result = new HashMap<>();
+        if ("subae".equals(key)) {
+            result = subaeService.pidExecute(hogi, pid, testVersion, floor, isfloor);
+        }
+
+        return  result;
+    }
+
 }
