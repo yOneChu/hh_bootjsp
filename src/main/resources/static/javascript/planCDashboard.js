17f833a405119b14c30c13a1f02a0513599cba74
kyhslam <kyhslam@gmail.com>
2026-02-06 22:03:39 +0900

    [2026-02-06] PLAN-C 대시보드 개발 중

--- src/main/resources/static/javascript/planCDashboard.js
+++ src/main/resources/static/javascript/planCDashboard.js
@@ -28,6 +28,11 @@ $(document).ready(function() {
     // Initial load
     searchPID();
 
+
+    chart();
+
+    lineChart();
+
 }); // end document ready
 
 
@@ -269,3 +274,173 @@ function reSetData() {
     hideLoading(); // 성공 시 로딩바 제거
 }
 
+function chart() {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'pie',
+            zooming: {
+                type: 'xy'
+            },
+            panning: {
+                enabled: true,
+                type: 'xy'
+            },
+            panKey: 'shift'
+        },
+        title: {
+            text: 'Egg Yolk Composition'
+        },
+        tooltip: {
+            valueSuffix: '%'
+        },
+        subtitle: {
+            text:
+                'Source:<a href="https://www.mdpi.com/2072-6643/11/3/684/htm" target="_default">MDPI</a>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: [{
+                    enabled: true,
+                    distance: 20
+                }, {
+                    enabled: true,
+                    distance: -40,
+                    format: '{point.percentage:.1f}%',
+                    style: {
+                        fontSize: '1.2em',
+                        textOutline: 'none',
+                        opacity: 0.7
+                    },
+                    filter: {
+                        operator: '>',
+                        property: 'percentage',
+                        value: 10
+                    }
+                }]
+            }
+        },
+        series: [
+            {
+                name: 'Percentage',
+                colorByPoint: true,
+                data: [
+                    {
+                        name: 'Water',
+                        y: 55.02
+                    },
+                    {
+                        name: 'Fat',
+                        sliced: true,
+                        selected: true,
+                        y: 26.71
+                    },
+                    {
+                        name: 'Carbohydrates',
+                        y: 1.09
+                    },
+                    {
+                        name: 'Protein',
+                        y: 15.5
+                    },
+                    {
+                        name: 'Ash',
+                        y: 1.68
+                    }
+                ]
+            }
+        ]
+    });
+}
+
+function lineChart() {
+    Highcharts.chart('container-02', {
+
+        title: {
+            text: 'U.S Solar Employment Growth',
+            align: 'left'
+        },
+
+        subtitle: {
+            text: 'By Job Category. Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>.',
+            align: 'left'
+        },
+
+        yAxis: {
+            title: {
+                text: 'Number of Employees'
+            }
+        },
+
+        xAxis: {
+            accessibility: {
+                rangeDescription: 'Range: 2010 to 2022'
+            }
+        },
+
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                },
+                pointStart: 2010
+            }
+        },
+
+        series: [{
+            name: 'Installation & Developers',
+            data: [
+                43934, 48656, 65165, 81827, 112143, 142383,
+                171533, 165174, 155157, 161454, 154610, 168960, 171558
+            ]
+        }, {
+            name: 'Manufacturing',
+            data: [
+                24916, 37941, 29742, 29851, 32490, 30282,
+                38121, 36885, 33726, 34243, 31050, 33099, 33473
+            ]
+        }, {
+            name: 'Sales & Distribution',
+            data: [
+                11744, 30000, 16005, 19771, 20185, 24377,
+                32147, 30912, 29243, 29213, 25663, 28978, 30618
+            ]
+        }, {
+            name: 'Operations & Maintenance',
+            data: [
+                null, null, null, null, null, null, null,
+                null, 11164, 11218, 10077, 12530, 16585
+            ]
+        }, {
+            name: 'Other',
+            data: [
+                21908, 5548, 8105, 11248, 8989, 11816, 18274,
+                17300, 13053, 11906, 10073, 11471, 11648
+            ]
+        }],
+
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+
+    });
+
+}
