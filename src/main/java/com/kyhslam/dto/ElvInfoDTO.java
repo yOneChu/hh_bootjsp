896d36abb7ebf02300e6abaf302758c87d954e7b
kyhslam <kyhslam@gmail.com>
2026-07-06 14:52:03 +0900

    [2026-07-06] api key 테스트

--- src/main/java/com/kyhslam/dto/ElvInfoDTO.java
+++ src/main/java/com/kyhslam/dto/ElvInfoDTO.java
@@ -13,28 +13,65 @@ public class ElvInfoDTO {
     private String PRODUCTNO;
     private String EL_AOPEN;
     private String EL_ECWBUFBH; // --CWT BUFFER BLOCKING 높이
-    private String EL_ECCH;//CAR 높이; CH
-    private String EL_ECBG;//CAR:BG
-    private String EL_ECEE;//CAR 무게중심;EE
-    private String EL_ECJJ;
-    private String EL_ERPW;
+    private String EL_ECCH; //CAR 높이; CH
+    private String EL_ECBG; //CAR:BG
+    private String EL_ECEE; //CAR 무게중심;EE
+    private String EL_ECJJ; //도어폭;JJ
+    private String EL_ECAA; //CAR 외부가로 ; AA
+    private String EL_ECBB; //CAR 외부세로 ; BB
+    private String EL_ECCA; //CAR 내부가로 ; CA
+    private String EL_ECCB; //CAR 내부세로 ; CB
+    private String EL_EHV; //승강로 세로;YY
+    private String EL_ECWBG; //CWT; BG
+    private String EL_ECWW;  //CWT;폭
+
+    private String EL_ECDOP; //◎ CAR DOOR OPER
+    private String EL_ERPW; //ROPE/BELT; 본수
+
+    private String EL_DCRG; //RGS적용
+    private String EL_BCDM; //도어재질
+    private String EL_BETM; //TRANSOM 재질/무늬
+    private String EL_BWALLT; //WALL구조
+    private String EL_BCLCDL; //LCD;취부위치
+    private String EL_BMOPB; //MAIN OPB 사양
+    private String EL_BOPBSWD; //OPB;SWING&WIDE
+
+    private String EL_ECWRL; //CWT RAIL(K)
+    private String EL_ETM; //권상기
+    private String EL_ETMD; //tm방향
+
+    private String EL_AFQ; //층수
+    private String EL_EHTRH;; //주행거리
 
 
-    private String EL_ECWRL;
-    private String EL_ETM;
-    private String EL_ECWBG;
-    private String EL_ECWW;
-    private String EL_ECSF;
-    private String EL_ASPC;
+    private String EL_ECSF; //CAR; SAFETY
+    private String EL_ASPC; //시방서
     private String EL_ASPCD;//시방서여부
     private String EL_BCL; // 천장종류
 
     private String EL_AMAN; //인승
-    private String EL_ASPSCD;
+    private String EL_ASPSCD; //생산거점(설계)
+    private String EL_ASPSC; //생산거점
     private String EL_ABRAND;
-    private String EL_ATYP;
-    private String EL_ASPD;
-    private String EL_ACAPA;
-    private String EL_AUSE;
+    private String EL_ATYP; //기종
+    private String EL_ASPD; //속도
+    private String EL_ACAPA; //용량
+    private String EL_AUSE; //용도
+
+
+    private String EL_ZTEXT_B; // --가내 특기사항
+    private String EL_ZTEXT_C; // --승장 특기사항
+    private String EL_ZTEXT_D; // --옵션 특기사항
+    private String EL_ZTEXT_E; // --L/O 특기사항
+    private String EL_ZERR_M3_1; // --기계 에러 메시지
+    private String EL_ZERR_E3_1; // --전기 에러 메시지
+    private String EL_ZERR_M5_1; // --기계 미품목,
+    private String EL_ZERR_E5_1; // --전기 미품목
+    private String EL_ZERR_C_1; // --공통 에러 메시지
+    private String EL_ZERR_A_1; // --자동 입력 오류
+    private String MD$USER; // -- 등록자
+    private String MD$CDATE; // --등록일
+    private String MANAGER_E; // --전기담당자
+    private String MANAGER_M; // --기계담당자
 
 }
--- src/main/java/com/kyhslam/util/ElvInfoCommonUtil.java
+++ src/main/java/com/kyhslam/util/ElvInfoCommonUtil.java
@@ -296,13 +296,11 @@ public class ElvInfoCommonUtil {
 
                 String vType =  obj.optString("TYPE");
 
-
                 if( !"3D_MODEL".equals(vType) ) {
                     resultList.add(map);
                 }
 
             }
-
 
             // 결과 출력
             /*for (HashMap<String, String> map : resultList) {
@@ -688,6 +686,166 @@ public class ElvInfoCommonUtil {
 
             pstmt = con.prepareStatement(sql.toString());
             //pstmt.setString(1, productOID);
+            //pstmt.setString(2, partNo);
+
+            rs = pstmt.executeQuery();
+
+            while(rs.next()) {
+                String PRODUCTNO = rs.getString("PRODUCTNO"); //제품번호
+                String OID = rs.getString("OID");
+                String STATUS = rs.getString("STATUS");
+                String EL_AOPEN = rs.getString("EL_AOPEN") == null ? "" : rs.getString("EL_AOPEN");
+                String EL_ECWBUFBH = rs.getString("EL_ECWBUFBH") == null ? "" : rs.getString("EL_ECWBUFBH");
+
+                String EL_ECCH = rs.getString("EL_ECCH") == null ? "" : rs.getString("EL_ECCH");
+                String EL_ECBG = rs.getString("EL_ECBG") == null ? "" : rs.getString("EL_ECBG");
+                String EL_ECEE = rs.getString("EL_ECEE") == null ? "" : rs.getString("EL_ECEE");
+                String EL_ECJJ = rs.getString("EL_ECJJ") == null ? "" : rs.getString("EL_ECJJ");
+                String EL_ERPW = rs.getString("EL_ERPW") == null ? "" : rs.getString("EL_ERPW");
+
+                String EL_ECWRL =  rs.getString("EL_ECWRL") == null ? "" : rs.getString("EL_ECWRL");
+                String EL_ETM =  rs.getString("EL_ETM") == null ? "" : rs.getString("EL_ETM");
+                String EL_ECWBG = rs.getString("EL_ECWBG") == null ? "" : rs.getString("EL_ECWBG");
+                String EL_ECWW = rs.getString("EL_ECWW") == null ? "" : rs.getString("EL_ECWW");
+                String EL_ECSF = rs.getString("EL_ECSF") == null ? "" : rs.getString("EL_ECSF");
+                String EL_ASPC =  rs.getString("EL_ASPC") == null ? "" : rs.getString("EL_ASPC");
+                String EL_ASPCD =  rs.getString("EL_ASPCD") == null ? "" : rs.getString("EL_ASPCD");
+                String EL_BCL =  rs.getString("EL_BCL") == null ? "" : rs.getString("EL_BCL");
+
+                String EL_AMAN = rs.getString("EL_AMAN") == null ? "" : rs.getString("EL_AMAN");
+                String EL_ASPSCD = rs.getString("EL_ASPSCD") == null ? "" : rs.getString("EL_ASPSCD");
+                String EL_ABRAND = rs.getString("EL_ABRAND") == null ? "" : rs.getString("EL_ABRAND");
+                String EL_ATYP = rs.getString("EL_ATYP") == null ? "" : rs.getString("EL_ATYP");
+                String EL_ASPD = rs.getString("EL_ASPD") == null ? "" : rs.getString("EL_ASPD");
+                String EL_ACAPA = rs.getString("EL_ACAPA") == null ? "" : rs.getString("EL_ACAPA");
+                String EL_AUSE =  rs.getString("EL_AUSE") == null ? "" : rs.getString("EL_AUSE");
+
+                ElvInfoDTO dto = new ElvInfoDTO();
+                dto.setPRODUCTNO(PRODUCTNO); //제품번호
+                dto.setProductoid(OID);
+                dto.setStatus(STATUS);
+                dto.setEL_AOPEN(EL_AOPEN);
+                dto.setEL_ECWBUFBH(EL_ECWBUFBH);
+                dto.setEL_ATYP(EL_ATYP);
+                dto.setEL_ECCH(EL_ECCH);
+                dto.setEL_ECBG(EL_ECBG);
+                dto.setEL_ECEE(EL_ECEE);
+                dto.setEL_ECJJ(EL_ECJJ);
+                dto.setEL_ERPW(EL_ERPW);
+
+                dto.setEL_ECWRL(EL_ECWRL);
+                dto.setEL_ETM(EL_ETM);
+                dto.setEL_ECWBG(EL_ECWBG);
+                dto.setEL_ECWW(EL_ECWW);
+                dto.setEL_ECSF(EL_ECSF);
+                dto.setEL_ASPC(EL_ASPC);
+                dto.setEL_ASPCD(EL_ASPCD);
+                dto.setEL_BCL(EL_BCL);
+                dto.setEL_AMAN(EL_AMAN);
+                dto.setEL_ASPSCD(EL_ASPSCD);
+                dto.setEL_ABRAND(EL_ABRAND);
+                dto.setEL_ATYP(EL_ATYP);
+                dto.setEL_ASPD(EL_ASPD);
+                dto.setEL_ACAPA(EL_ACAPA);
+                dto.setEL_AUSE(EL_AUSE);
+
+                dataList.add(dto);
+            } //end while
+
+            System.out.println("dataList.size() = " + dataList.size());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            PLMDBConnection.disconnect(con, pstmt, rs);
+        }
+
+        return dataList;
+    }
+
+
+    //api용 특정호기의 영업사양 정보 조회
+    public static ArrayList<ElvInfoDTO> findElvInfoList(String hogi) {
+        Connection con 			= null;
+        PreparedStatement pstmt = null;
+        ResultSet rs 			= null;
+
+        ArrayList<ElvInfoDTO> dataList = new ArrayList<ElvInfoDTO>();
+
+        try {
+            con = PLMDBConnection.getConnection();
+
+            String sql = """        
+                     SELECT V.MD$DESC, 
+                     V.MD$NUMBER AS PRODUCTNO,
+                     V.VF$OUID AS OID,
+                     COD(V.EL_AOPEN) AS EL_AOPEN,
+                     V.MD$STATUS AS STATUS,
+                     CODN(v.EL_AUSE) AS EL_AUSE, -- 용도
+                     CODN(V.EL_ETM) AS EL_ETM, --권상기
+                     V.EL_ECWBUFBH, --CWT BUFFER BLOCKING 높이 
+                     V.EL_ECCH, --CAR 높이; CH 
+                     V.EL_ECBG, --CAR:BG 
+                     V.EL_ECEE, --CAR 무게중심;EE 
+                     V.EL_ECAA, --CAR 외부가로 ; AA      
+                     V.EL_ECBB, --CAR 외부세로 ; BB      
+                     V.EL_ECCA, --CAR 내부가로 ; CA      
+                     V.EL_ECCB, --CAR 내부세로 ; CB
+                     V.EL_ECJJ, --도어폭;JJ       
+                     V.EL_ECDOP, --◎ CAR DOOR OPER
+                     V.EL_ERPW, -- ROPE/BELT; 본수
+                     COD(V.EL_ECWRL) AS EL_ECWRL, --CWT RAIL(K) 
+                     COD(V.EL_ETM) AS EL_ETM, --권상기 
+                     V.EL_ECWBG AS EL_ECWBG, --CWT; BG 
+                     V.EL_ECWW AS EL_ECWW, --CWT;폭 
+                     COD(V.EL_ECSF) AS EL_ECSF, --CAR; SAFETY 
+                     COD(V.EL_ASPC) AS EL_ASPC, --시방서 
+                     COD(V.EL_ASPCD) AS EL_ASPCD, -- 시방서 DEVIATION 여부 
+                     COD(V.EL_BCL) AS EL_BCL, -- 천장종류 
+                     V.EL_AMAN AS EL_AMAN, --인승 
+                     COD(V.EL_ASPSCD) AS EL_ASPSCD, --생산거점(설계) 
+                     CONCAT('elv_info$vf@', LOWER(DECTOHEX(V.vf$ouid))) OUID,   -- 영업사양 객체 
+                     CODN(V.EL_ABRAND) AS EL_ABRAND, -- 브랜드 
+                     CODN(V.EL_ATYP) AS EL_ATYP, -- 기종 
+                     CODN (V.EL_ASPD) AS EL_ASPD, -- 속도 
+                     CODN (V.EL_ACAPA) AS EL_ACAPA, --용량 
+                     COD(V.EL_BCDM) AS EL_BCDM, --도어재질 
+                     COD(V.EL_BWALLT) AS EL_BWALLT, --WALL구조 
+                     COD(V.EL_BCLCDL) AS EL_BCLCDL, --    LCD;취부위치 
+                     COD(V.EL_BMOPB) AS EL_BMOPB, -- MAIN OPB사양
+                     COD(V.EL_BETM) AS EL_BETM, --TRANSOM 재질/무늬 
+                     COD(V.EL_BOPBSWD) AS EL_BOPBSWD, --  OPB;SWING&WIDE
+                     V.EL_AFQ, --층수, 
+                     V.EL_EHTRH,--주행거리 
+                     V.EL_EHV, -- 승강로 세로;YY 
+                     V.EL_ZTEXT_B, --가내 특기사항 
+                     V.EL_ZTEXT_C, --승장 특기사항 
+                     V.EL_ZTEXT_D, --옵션 특기사항 
+                     V.EL_ZTEXT_E, --L/O 특기사항 
+                     V.EL_ZERR_M3_1, --기계 에러 메시지 
+                     V.EL_ZERR_E3_1, --전기 에러 메시지 
+                     V.EL_ZERR_M5_1, --기계 미품목, 
+                     V.EL_ZERR_E5_1, --전기 미품목 
+                     V.EL_ZERR_C_1, --공통 에러 메시지 
+                     V.EL_ZERR_A_1, --자동 입력 오류 
+                     V.MD$USER, --등록자
+                     V.MD$CDATE,  --등록일
+                     V.MANAGER_E, --전기담당자 
+                     V.MANAGER_M  --기계담당자 
+                     --,V.* 
+              FROM ELV_INFO$VF V, ELV_INFO$ID A 
+              WHERE 
+                  V.vf$identity = A.id$ouid and V.vf$ouid = A.id$wip 
+                  AND V.MD$NUMBER NOT LIKE 'Q%'
+                  AND V.MD$NUMBER NOT LIKE '%TEST%'
+                  AND V.MD$NUMBER = ?
+                """;
+
+
+            System.out.println("sql = " + sql);
+
+            pstmt = con.prepareStatement(sql.toString());
+            pstmt.setString(1, hogi);
             //pstmt.setString(2, partNo);
 
             rs = pstmt.executeQuery();
